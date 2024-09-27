a43d547eef61e62c4d104d1cf76df4ff3e914b7a
50chin <50chin50@gmail.com>
2024-09-27 18:58:41 +0300

    Updates

---
+++ addToOrder.js
@@ -0,0 +1,23 @@
+export const addToOrder = [
+  {
+    id: 1,
+    img: './img/addToOrder/1.png',
+    name: 'Молочный коктейль с...',
+    price: 175,
+    quantity: 1,
+  },
+  {
+    id: 2,
+    img: './img/addToOrder/2.png',
+    name: 'Рулетики с брусникой',
+    price: 195,
+    quantity: 1,
+  },
+  {
+    id: 3,
+    img: './img/addToOrder/3.png',
+    name: 'Картофель из печи',
+    price: 205,
+    quantity: 1,
+  },
+];
---
+++ ingredients.js
@@ -0,0 +1,47 @@
+export const ingredients = [
+  {
+    img: './img/modalIngredients/bort.png',
+    title: 'Сырный бортик',
+    price: 199,
+  },
+  {
+    img: './img/modalIngredients/mushrooms.png',
+    title: 'Шампиньоны',
+    price: 39,
+  },
+  {
+    img: './img/modalIngredients/chicken.png',
+    title: 'Цыпленок',
+    price: 59,
+  },
+  {
+    img: './img/modalIngredients/cheese.png',
+    title: 'Кубики брынзы',
+    price: 79,
+  },
+  {
+    img: './img/modalIngredients/pepper.png',
+    title: 'Острый перец халапеньо',
+    price: 59,
+  },
+  {
+    img: './img/modalIngredients/6.png',
+    title: 'Нежный цыпленок',
+    price: 99,
+  },
+  {
+    img: './img/modalIngredients/mozzarella.png',
+    title: 'Сливочная моцарелла',
+    price: 79,
+  },
+  {
+    img: './img/modalIngredients/cheddar.png',
+    title: 'Сыры чеддер и пармезан',
+    price: 79,
+  },
+  {
+    img: './img/modalIngredients/cucumbers.png',
+    title: 'Маринованные огурчики',
+    price: 59,
+  },
+];
